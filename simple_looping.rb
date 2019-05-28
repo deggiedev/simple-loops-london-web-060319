bb82bec5bf739971d02451ca54d73b3f4d6dc4fc
Jack de Grave <jack.degrave@outlook.com>
2019-05-28 13:55:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -29,8 +29,9 @@ def while_iterator(number_of_times)
   counter = 0 
   counter += 1
   while counter < number_of_times
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  puts phrase
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
 def until_iterator(number_of_times)
