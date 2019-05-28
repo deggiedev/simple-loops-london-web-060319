df66d0995fb509c6b904d4882ea0ad8a69987518
Jack de Grave <jack.degrave@outlook.com>
2019-05-28 14:02:57 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -44,10 +44,11 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for 
+  for 1..#{number_of_times}
+  puts phrase
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+end
 end
 
