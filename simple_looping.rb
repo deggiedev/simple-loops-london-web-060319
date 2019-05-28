0a6d652632c2e8eefa1568ba39289ed85140abe2
Jack de Grave <jack.degrave@outlook.com>
2019-05-28 13:47:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def loop_iterator(number_of_times)
     if counter <= #{number_of_times}
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
-  end
+  
 end
 
 def times_iterator(number_of_times)
